55fcd1ce20a275aef45d896982cd9fae6c8f1f6a
fedelman <traptsud@gmail.com>
2024-03-10 21:38:51 +0300

    Добавил решение Task 002

---
+++ Task_002/Program.cs
@@ -0,0 +1,13 @@
+﻿void Main() 
+{
+     Console.WriteLine(dz2(3, 2));
+}
+
+int dz2(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (n == 0) return dz2(m - 1, 1);
+    return dz2(m - 1, dz2(m, n - 1));
+}
+
+Main();
