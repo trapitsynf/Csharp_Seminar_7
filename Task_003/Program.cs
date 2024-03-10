83dc40165032a6267366d73ae421c0d29fafdbc4
fedelman <traptsud@gmail.com>
2024-03-10 21:42:11 +0300

    Добавил решение Task 003

---
+++ Task_003/Program.cs
@@ -0,0 +1,13 @@
+﻿void Main()
+{
+    int[] arr = [1678, 24, 345, 46, 50];
+    dz3(arr, arr.Length - 1);
+}
+
+void dz3(int[] nums, int n) {
+    if (n < 0) return;
+    Console.Write(nums[n] + " ");
+    dz3(nums, n - 1);
+}
+
+Main();
